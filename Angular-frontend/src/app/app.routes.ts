981cb2f96deca498f3483c45f7ca657cbc097295
David Segerbo <david.k.segerbo@gmail.com>
2025-05-01 03:36:04 +0200

    updated

---
+++ Angular-frontend/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { NewPageComponent } from './new-page/new-page.component';
+
+export const routes: Routes = [
+    { path: 'new-page', component: NewPageComponent },
+];
